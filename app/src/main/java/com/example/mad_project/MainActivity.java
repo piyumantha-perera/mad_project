7bd13db5f3d33db6fe30b90ce0a87f3bfe68425c
piyumantha perera <41514689+piyumantha-perera@users.noreply.github.com>
2020-03-07 03:06:41 -0800

    home new, login

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView3);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
