d409587e631885ac5747da8125a15aacc9419a4f
RavinduDilhara <ravindu.1dilhara@gmail.com>
2020-03-11 16:19:50 +0530

    new update admin choose

--- app/src/main/java/com/example/mad_project/Admin_Login.java
+++ app/src/main/java/com/example/mad_project/Admin_Login.java
@@ -2,13 +2,26 @@ package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Admin_Login extends AppCompatActivity {
 
+    Button adlog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__login);
+
+        adlog = findViewById(R.id.buttonAdminLogin);
+        adlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Admin_Login.this,Admin_Choose.class);
+                startActivity(intent);
+            }
+        });
     }
 }
