0d5db2525f28db09f4d15ecbcf275677712e54cf
RavinduDilhara <ravindu.1dilhara@gmail.com>
2020-03-18 14:12:03 +0530

    customer profile

--- app/src/main/java/com/example/mad_project/Customer_Login.java
+++ app/src/main/java/com/example/mad_project/Customer_Login.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Customer_Login extends AppCompatActivity {
 
     TextView register;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,19 @@ public class Customer_Login extends AppCompatActivity {
         setContentView(R.layout.activity_customer_login);
 
         register = findViewById(R.id.textViewLoginGoReg);
+        login = findViewById(R.id.buttonCusLogin);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Customer_Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Customer_Login.this,Customer_Profile.class);
                 startActivity(intent);
             }
         });
