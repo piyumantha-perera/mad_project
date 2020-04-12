0025d13da330113eff310d92ddb3a80642fe2c07
RavinduDilhara <ravindu.1dilhara@gmail.com>
2020-04-12 11:14:41 +0530

    profile view

--- app/src/main/java/com/example/mad_project/Customer_Profile.java
+++ app/src/main/java/com/example/mad_project/Customer_Profile.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Customer_Profile extends AppCompatActivity {
 
     Button creation;
+    TextView user, usrEmail, usrContact, usrAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +19,21 @@ public class Customer_Profile extends AppCompatActivity {
         setContentView(R.layout.activity_customer__profile);
         creation = findViewById(R.id.buttonProfileCreation);
 
+        user = findViewById(R.id.textViewCusPUsername);
+        usrEmail = findViewById(R.id.textViewCusPEmail);
+        usrContact = findViewById(R.id.textViewCusPContact);
+        usrAddress = findViewById(R.id.textViewCusPCompany);
+
         Bundle bn = getIntent().getExtras();
         final String username = bn.getString("Name");
+        final String email = bn.getString("Email");
+        final String contactNo = bn.getString("ContactNo");
+        final String address = bn.getString("Address");
+
+        user.setText(username);
+        usrEmail.setText(email);
+        usrContact.setText(contactNo);
+        usrAddress.setText(address);
 
         creation.setOnClickListener(new View.OnClickListener() {
             @Override
