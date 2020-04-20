d990a9828b822ada7787e18687ecef9ae70df63d
piyumantha perera <41514689+piyumantha-perera@users.noreply.github.com>
2020-04-20 12:24:29 -0700

    error fixed

---
+++ app/src/main/java/com/example/mad_project/Employee_Details/Salary_View.java
@@ -0,0 +1,18 @@
+package com.example.mad_project.Employee_Details;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mad_project.R;
+
+public class Salary_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salary__view);
+
+
+    }
+}
